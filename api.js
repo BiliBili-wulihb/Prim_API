cdd6168c2a75eb8866ded67371517b100d38e3dc
BiliBili-wulihb <102204002+BiliBili-wulihb@users.noreply.github.com>
2022-03-23 12:27:18 +0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,15 @@
+/* 引入外部css */
+importCssJs.css('style.css');
+/*API链接样式*/
+api{
+  background-color:orange;
+  color:white;
+  cursor:pointer;
+  text-decoration:underline;
+}
+/*普通链接样式(请使用JS跳转)*/
+.link{
+  color:blue;
+  text-decoration:underline;
+  cursor:pointer;
+}
