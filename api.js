a347ab7ffa1f6fe006e96c550cce3196fbaea7b8
BiliBili-wulihb <102204002+BiliBili-wulihb@users.noreply.github.com>
2022-03-23 13:03:25 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
-/* 引入外部css */
-importCssJs.css('style.css');
-/*跳转插件*/
+/*测试插件模块*/
+document.getElementById("csdn").innerHTML="Hello World"
+/*跳转模块*/
 function golink(){
   window.location.href=link;
 }
