127ec390596a6ac8d76430dee3068e0222575bb5
BiliBili-wulihb <102204002+BiliBili-wulihb@users.noreply.github.com>
2022-03-23 13:06:18 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 /*测试插件模块*/
-document.getElementById("csdn").innerHTML="Hello World"
+document.getElementById("test_plugin").innerHTML="Hello World"
 /*跳转模块*/
 function golink(){
   window.location.href=link;
