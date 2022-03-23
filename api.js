8a6e959105b3006b1af591bf040f75b0cef448de
BiliBili-wulihb <102204002+BiliBili-wulihb@users.noreply.github.com>
2022-03-23 12:29:04 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,15 +1,6 @@
 /* 引入外部css */
 importCssJs.css('style.css');
-/*API链接样式*/
-api{
-  background-color:orange;
-  color:white;
-  cursor:pointer;
-  text-decoration:underline;
-}
-/*普通链接样式(请使用JS跳转)*/
-.link{
-  color:blue;
-  text-decoration:underline;
-  cursor:pointer;
+/*跳转插件*/
+function golink(){
+  window.location.href=link;
 }
